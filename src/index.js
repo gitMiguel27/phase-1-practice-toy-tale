7184a7e346b303cb8f0108dd2b6338fc62ef9885
Miguel Nazario <mnazario35@gmail.com>
2022-02-09 17:37:48 -0500

    commit tree dbb76a924f2fda0196c063f62a0526be5852b0e3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 let addToy = false;
+let toyCollection;
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +14,73 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function setUpForm(){
+  let formToyName = document.getElementById('form-toy-name');
+  let formToyImage = document.getElementById('form-toy-image');
+  let formSubmit = document.getElementById('form-toy-submit');
+  
+  formSubmit.addEventListener('click', (event) => {
+    let toy = {
+      id: toyCollection.length,
+      name: formToyName.value,
+      image: formToyImage.value,
+      likes: 0
+    }
+    createToyCard(toy);
+
+    toyCollection.push(toy);
+  });
+}
+
+function getAllToys() {
+  fetch("http://localhost:3000/toys")
+  .then(response => response.json())
+  .then(json => {
+    console.log(json);
+    toyCollection = json;
+    json.forEach(toy => {
+      createToyCard(toy);
+    });
+
+  })
+}
+
+function createToyCard(toy) {
+  let toyDiv = document.createElement('div');
+  toyDiv.className = 'card';
+
+  let toyName = document.createElement('h2');
+  toyName.textContent = toy.name;
+
+  let toyImage = document.createElement('img');
+  toyImage.src = toy.image;
+  toyImage.className = 'toy-avatar';
+
+  let toyLikes = document.createElement('p');
+  toyLikes.textContent = toy.likes;
+
+  let toyLikeButton = document.createElement('button');
+  toyLikeButton.className = 'like-btn';
+  toyLikeButton.textContent = 'Like ❤️';
+  toyLikeButton.id = toy.id;
+  toyLikeButton.addEventListener('click', event => {
+    console.log('clicked like on ' + toy.name);
+
+    toy.likes = toy.likes + 1;
+    toyLikes.textContent = toy.likes;
+  });
+
+  toyDiv.appendChild(toyName);
+  toyDiv.appendChild(toyImage);
+  toyDiv.appendChild(toyLikes);
+  toyDiv.appendChild(toyLikeButton);
+  // Can also use append(); with appropriate order 
+  // toyDiv.append(toyName, toyImage, toyLikes, toyLikeButton);
+
+  let toyCollection = document.getElementById('toy-collection');
+  toyCollection.appendChild(toyDiv);
+}
+
+getAllToys();
+setUpForm();
